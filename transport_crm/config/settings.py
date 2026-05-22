f1c4183db7160bedf395f2e48656449a669cd95d
novovaytsk-dev <novovaytsk@gmail.com>
2026-05-22 20:19:41 +0300

    настройка уведомлений

--- transport_crm/config/settings.py
+++ transport_crm/config/settings.py
@@ -134,9 +134,9 @@ YANDEX_STATIC_API_KEY = '72491f37-0a15-4d26-81e7-17fd201d6115'
 
 # --- Настройки почты (для разработки используем консольный бэкенд) ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'al.mari4eff@yandex.ru'
-EMAIL_HOST_PASSWORD = 'rlnqzhpcwxyjoope' #rlnqzhpcwxyjoope
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ac3a3d001@smtp-brevo.com'
+EMAIL_HOST_PASSWORD = 'bsks27ajUfjrWF9' #bsks27ajUfjrWF9
+DEFAULT_FROM_EMAIL = 'al.mari4eff@yandex.ru'
