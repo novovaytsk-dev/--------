e10cabb9d43921f65050eea04cb6c80207e88473
novovaytsk-dev <novovaytsk@gmail.com>
2026-06-06 12:44:23 +0300

    Документирование кода

--- transport_crm/crm/bot.py
+++ transport_crm/crm/bot.py
@@ -1,10 +1,25 @@
 import requests
 from django.conf import settings
 
-TOKEN = '8611685364:AAGkf8eFYKvy-8DhxuEqtU4k1JbPYrxBSFs'
+"""
+Модуль для отправки уведомлений в Telegram через HTTP-запросы.
+Использует токен бота, полученный от BotFather.
+"""
 
-def send_telegram_message(telegram_id, text):
-    """Отправляет текстовое сообщение в Telegram."""
+
+TOKEN: str = '8611685364:AAGkf8eFYKvy-8DhxuEqtU4k1JbPYrxBSFs'
+
+def send_telegram_message(telegram_id: int, text: str) -> None:
+    """
+    Отправляет текстовое сообщение пользователю Telegram.
+
+    Args:
+        telegram_id (int): ID чата получателя.
+        text (str): Текст сообщения.
+
+    Returns:
+        None: В случае ошибки выводит сообщение в консоль.
+    """
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {
         'chat_id': telegram_id,
